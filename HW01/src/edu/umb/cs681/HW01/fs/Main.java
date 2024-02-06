d574af91e8721d09c3b7d3c4117eb6885cb2f33b
Pratik0116 <125710655+Pratik0116@users.noreply.github.com>
2024-02-06 16:00:13 -0500

    Add files via upload

---
+++ HW01/src/edu/umb/cs681/HW01/fs/Main.java
@@ -0,0 +1,30 @@
+package edu.umb.cs681.HW01.fs;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.IntSummaryStatistics;
+import java.util.Map;
+
+import static edu.umb.cs681.HW01.fs.FileStructure.createFS;
+
+public class Main {
+    public static void main(String[] args) {
+        FileSystem fs = createFS();
+        LocalDateTime threshold = LocalDateTime.of(2024, Month.FEBRUARY, 1, 05, 10);
+        String extension = ".class";
+        int count = fs.countFilesAfter(threshold, extension);
+        System.out.println("Number of files created after " + threshold + " with extension " + extension + ": " + count);
+
+        Map<String, IntSummaryStatistics> groupedFileStats = fs.groupFilesByExtensionWithStats();
+        for (Map.Entry<String, IntSummaryStatistics> entry : groupedFileStats.entrySet()) {
+            extension = entry.getKey();
+            IntSummaryStatistics stats = entry.getValue();
+            System.out.println("Extension: " + extension);
+            System.out.println("-> Count: " + stats.getCount());
+            System.out.println("-> Total size: " + stats.getSum());
+            System.out.println("-> Average size: " + stats.getAverage());
+            System.out.println("-> Min size: " + stats.getMin());
+            System.out.println("-> Max size: " + stats.getMax());
+        }
+    }
+}
